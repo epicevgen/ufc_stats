7d7edce99fff204034a6d54bb7d3509e51ebe8c5
Евгений Столяров <epicevgen@gmail.com>
2025-08-24 16:04:14 +0300

    create project

---
+++ src/main/java/com/example/ufc_stats/UfcStatsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ufc_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UfcStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UfcStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ufc_stats/UfcStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ufc_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UfcStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
